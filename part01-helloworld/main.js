6766ec202d4760e2cb79a879113e0be61cd2e8be
thenabiev <sahinnabee@gmail.com>
2023-02-17 23:17:57 +0400

    .

--- part01-helloworld/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var message = "Hello World";
-var sayHi = function () {
-    console.log("Hi!");
-};
-sayHi();
-console.log(message);
--- part04-interface/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function fullName(person) {
-    console.log("".concat(person.firstName, " ").concat(person.lastName));
-}
-var p = {
-    firstName: "Su-57"
-};
-fullName(p);
